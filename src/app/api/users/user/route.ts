91c84f7be29a0afbb5efbebcb5b4b94abbb35a30
Msaketh7 <msaketh7@gmail.com>
2025-07-17 21:48:11 -0500

    added middleware and profile

---
+++ src/app/api/users/user/route.ts
@@ -0,0 +1,26 @@
+import { getDataFromToken } from "@/helpers/getDataFromToken";
+import { NextRequest, NextResponse } from "next/server";
+import User from "@/models/userModel";
+import { connect } from "@/dbConfig/dbConfig";
+
+connect();
+
+export async function GET(request:NextRequest) {
+    try {
+        const userId = getDataFromToken(request);
+        const user = await User.findById(userId).select("-password");
+
+        return NextResponse.json({
+            message: "User found",
+            data: user,
+        });
+
+        if (!user) {
+            return NextResponse.json({ error: "User not found" }, { status: 404 });
+        }
+
+        return NextResponse.json({ user, success: true });
+    } catch (error: any) {
+        return NextResponse.json({ error: error.message }, { status: 500 });
+    }
+}
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,9 +1,53 @@
+"use client";
+import axios from 'axios';
+import Link from 'next/link';
+import React, {useState} from 'react';
+import { useRouter } from 'next/navigation';
+import toast from 'react-hot-toast';
+
+
+
 export default function ProfilePage() {
+    const router = useRouter();
+    const [data, setData] = React.useState("nothing");
+
+    const logout = async () => {
+        try {
+            const response = await axios.get('/api/users/logout');
+            console.log(response.data);
+            toast.success('Logout successful');
+            router.push('/login');
+        } catch (error: any) {
+            console.log(error.message);
+            toast.error(error.message);
+        }
+    }
+
+    const getUserDetails = async () => {
+        const res = await axios.get('/api/users/user');
+        console.log(res.data);
+        setData(res.data.data._id);
+        toast.success('User details fetched successfully');
+    }
+
     return (
         <div className="flex flex-col items-center justify-center min-h-screen bg-black-100">
             <h1>Profile</h1>
             <hr />
             <p>Profile Page</p>
+            <h2 className="text-gray-900 font-mono text-2xl mb-4 font-bold tracking-wider outline outline-offset-2">
+                {data === "nothing" ? "Nothing" : <Link href={`/profile/${data}`}>{data}</Link>}
+            </h2>
+            <hr />
+            <button 
+            onClick={getUserDetails}
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+               getUserDetails</button>
+
+            <button 
+            onClick={logout}
+            className="bg-red-500 hover:bg-blue-700 text-white font-bold my-5 py-2 px-4 rounded">
+                Logout</button>
         </div>
     )
 }

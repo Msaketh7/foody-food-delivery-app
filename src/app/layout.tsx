ca3fadb0e2fc82e2bfffa4e9a682304983f7c4e1
Msaketh7 <msaketh7@gmail.com>
2025-07-23 22:58:41 -0500

    deploy insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
 import { ThemeProvider } from "./context/themeprovider";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,6 +34,7 @@ export default function RootLayout({
         <Toaster />
         {children}
         </ThemeProvider>
+        <SpeedInsights/>
       </body>
     </html>
   );
